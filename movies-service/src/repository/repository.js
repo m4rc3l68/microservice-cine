9974ea7144ba16a2be3b4b373eef8b6241178799
marcel <marcelinocost69@gmail.com>
2023-04-30 20:12:24 -0300

    commit - 04

--- movies-service/src/repository/repository.js
+++ movies-service/src/repository/repository.js
@@ -6,21 +6,21 @@ async function getAllMovies() {
   return db.collection('movies').find().toArray()
 }
 
-// async function getMoviesById(id) {
-//   const db = database.connect()
-//   return db.collection('movies').find({ _id: new ObjectId(id) })
-// }
+async function getMoviesById(id) {
+  const db = database.connect()
+  return db.collection('movies').find({ _id: new ObjectId(id) })
+}
 
-// async function getMoviesPremieres() {
-//   const monthAgo = new Date()
-//   monthAgo.setMonth(-1)
+async function getMoviesPremieres() {
+  const monthAgo = new Date()
+  monthAgo.setMonth(-1)
 
-//   const db = await database.connect()
-//   return db.collection('movies').find({ dataLacamento: { $gte: monthAgo } }).toArray()
-// }
+  const db = await database.connect()
+  return db.collection('movies').find({ dataLacamento: { $gte: monthAgo } }).toArray()
+}
 
 module.exports = {
-  getAllMovies,
-  // getMoviesById,
-  // getMoviesPremieres
+  getAllMovies, 
+  getMoviesById,
+  getMoviesPremieres
 }
--- movies-service/src/repository/repository.test.js
+++ movies-service/src/repository/repository.test.js
@@ -4,3 +4,11 @@ const repo = require('./repository')
 test('Obtain Movies', repo.getAllMovies, () => {
  expect(repo).toBeTruthy()
 })
+
+test('Obtain ID Movies', repo.getMoviesById, () => {
+  expect(repo).toBeTruthy()
+})
+
+test('Obtain Premieres', repo.getMoviesPremieres, () => {
+  expect(repo).toBeTruthy()
+})
