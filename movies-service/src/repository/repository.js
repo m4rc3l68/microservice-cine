983f9205c4f5a9164fdb1313c391db3d713612ce
marcel <marcelinocost69@gmail.com>
2023-04-30 18:52:00 -0300

    commit - 02

---
+++ movies-service/src/repository/repository.js
@@ -0,0 +1,26 @@
+const { ObjectId } = require('mongodb')
+const database = require('../config/database')
+
+async function getAllMovies() {
+  const db = await database.connect()
+  return db.collection('movies').find().toArray()
+}
+
+async function getMoviesById(id) {
+  const db = database.connect()
+  return db.collection('movies').find({ _id: new ObjectId(id) })
+}
+
+async function getMoviesPremieres() {
+  const monthAgo = new Date()
+  monthAgo.setMonth(-1)
+
+  const db = await database.connect()
+  return db.collection('movies').find({ dataLacamento: { $gte: monthAgo } }).toArray()
+}
+
+module.exports = {
+  getAllMovies,
+  getMoviesById,
+  getMoviesPremieres
+}
