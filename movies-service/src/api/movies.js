b4b1b2e4ea883b41417fe00c379e33ae9d822dc4
marcel <marcelinocost69@gmail.com>
2023-05-03 11:45:47 -0300

    commit - 14

---
+++ movies-service/src/api/movies.js
@@ -0,0 +1,23 @@
+module.exports = (app, repository) => {
+  app.get('/movies/premiers', async (req, res, next) => {
+    const movies = await repository.getMoviesPremieres()
+    if(!movies || !movies.length) return res.sendStatus(404)
+
+    res.json(movies)
+  })
+
+  app.get('/movies:id', async (req, res, next) => {
+    const movie = await repository.getMovieById(req.params.id)
+    if(!movie) return res.sendStatus(404)
+
+    res.json(movie)
+  })
+
+  app.get('/movies', async (req, res, next) => {
+    const movies = await repository.getAllMovies()
+    if(!movies || !movies.length) return res.sendStatus(404)
+
+    res.json(movies)
+  })
+
+}
--- movies-service/src/server/server.test.js
+++ movies-service/src/server/server.test.js
@@ -2,19 +2,21 @@ const { test, expect } = require('@jest/globals')
 const server = require('./server')
 const request = require('supertest')
 
+const apiMock = jest.fn((app, repository) => true)
+
 test('Server Start', async () => {
-  const app = await server.start()
+  const app = await server.start(apiMock)
  expect(app).toBeTruthy()
+})
+
+test('Health Check', async () => {
+  process.env.PORT = 3001
+  const app = await server.start(apiMock)
+  const response = await request(app).get('/health')
+  expect(response.status).toEqual(200)
 })
 
 test('Server Stop', async () => {
   const isStopped = await server.stop()
   expect(isStopped).toBeTruthy()
 })
-
-test('Health Check', async () => {
-  const app = await server.start()
-  const response = await request(app).get('/health')
-  expect(response.status).toEqual(200)
-})
-
