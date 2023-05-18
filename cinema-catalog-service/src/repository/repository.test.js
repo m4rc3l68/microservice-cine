7b35b3a5be0db34e17fbb69b01790833d5f2823a
marcel <marcelinocost69@gmail.com>
2023-05-17 21:21:24 -0300

    commit - 05

--- cinema-catalog-service/src/repository/repository.test.js
+++ cinema-catalog-service/src/repository/repository.test.js
@@ -2,43 +2,13 @@ const { test, expect } = require('@jest/globals')
 const repository = require('./repository')
 const disconnectRepo = require('../config/database')
 
-let testMovieId = null
-
 beforeAll( async () => {
-  const movies = await repository.getAllMovies()
-  testMovieId = movies[0]._id
+
 })
 
-test('getAllMovies', async () => {
-  const movies = await repository.getAllMovies()
- expect(Array.isArray(movies)).toBeTruthy()
- expect(movies.length).toBeTruthy()
+test('getAllCities', async () => {
+  const cities = await repository.getAllCities()
+  console.log(cities)
+  expect(Array.isArray(cities)).toBeTruthy()
+  expect(cities.length).toBeTruthy()
 })
-
-test('getMovieById', async () => {
-  const movie = await repository.getMovieById(testMovieId)
-  expect(movie).toBeTruthy()
-  // expect(movie._id).toEqual(testMovieId) 
-})
-
-test('getMoviesPremieres', async () => {
-  const monthAgo = new Date()
-  monthAgo.setMonth(-1)
-
-  const movies = await repository.getMoviesPremieres()
-  expect(Array.isArray(movies)).toBeTruthy()
-  expect(movies.length).toBeTruthy()
-  expect(movies[0].dataLancamento.getTime()).toBeGreaterThanOrEqual(monthAgo.getTime())
-})
-
-test('Disconnecting Repoditory', async () => {
-  const isDisconnected  = await disconnectRepo.disconnect()
-  expect(isDisconnected).toBeTruthy()
-})
-
-test('Disconnecting Repository 2x', async () => {
-  await disconnectRepo.disconnect()
-  const isDisconnected  = await disconnectRepo.disconnect()
-  expect(isDisconnected).toBeTruthy()
-})
- 
