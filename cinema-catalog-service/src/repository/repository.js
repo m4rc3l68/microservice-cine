7e62209da17c0924bf2964e0ba300952db6b2759
marcel <marcelinocost69@gmail.com>
2023-06-06 20:44:25 -0300

    commit - 10

--- cinema-catalog-service/src/repository/repository.js
+++ cinema-catalog-service/src/repository/repository.js
@@ -9,14 +9,40 @@ async function getAllCities() {
     .toArray()
 }
 
-async function getCinemasCityById(cityId){
-  const db = await database.connect()
+async function getCinemasByCityId(cityId) {
   const objCityId = new ObjectId(cityId)
-  return db.collection('cinemaCatalog')
+  const db = await database.connect()
+  const city = await db.collection('cinemaCatalog')
     .findOne({ _id: objCityId}, {projection: {cinemas: 1}})
+
+  
+
+  return city.cinemas  
+}
+/**
+ * 
+ * { id: 1, frutas ["maçã, "laranja] }, { id: 2, frutas ["laranja", "banana"] }
+ * { id: 1, frutas: "maçã"}, {id: 1, frutas: "laranja"}, { id: 2, frutas: "laranja"}, {id: 2, frutas: "banana"}
+ * 
+ */
+
+async function getMoviesByCinemaId(cinemaId) {
+  const db = await database.connect()
+  const objCinemaId = new ObjectId(cinemaId)
+  const group = await db.collection('cinemaCatalog').aggregate([
+    { $match: { "cinemas._id": objCinemaId } },
+    { $unwind: "$cinemas" },
+    { $unwind: "$cinemas.salas" },
+    { $unwind: "$cinemas.salas.sessoes" },
+    { $group: { _id: { titulo: "$cinemas.salas.sessoes.filme", _id: "$cinemas.salas.sessoes.idFilme"}} }
+  ]).toArray()
+
+  return group.map(g => g._id)
 }
 
 module.exports = {
   getAllCities,
-  getCinemasCityById, 
+  getCinemasByCityId, 
+  getMoviesByCinemaId,
 }
+
--- cinema-catalog-service/src/repository/repository.test.js
+++ cinema-catalog-service/src/repository/repository.test.js
@@ -2,11 +2,15 @@ const { test, expect } = require('@jest/globals')
 const repository = require('./repository')
 //const disconnectRepo = require('../config/database')
 let cityId = null
+let cinemaId = null
 
 beforeAll( async () => {
   const cities = await repository.getAllCities()
-  cityId = cities[0]._id
-})
+  cityId = cities[cities.length - 1]._id
+
+  const cinemas = await repository.getCinemasByCityId(cityId)
+  cinemaId = cinemas[0]._id
+}) 
 
 test('getAllCities', async () => {
   const cities = await repository.getAllCities()
@@ -14,9 +18,15 @@ test('getAllCities', async () => {
   expect(cities.length).toBeTruthy()
 })
 
-test('getCinemasCityById', async () => {
-  const city = await repository.getCinemasCityById(cityId)
-  console.log(city)
-  expect(city).toBeTruthy()
-  expect(Array.isArray(city.cinemas)).toBeTruthy()
+test('getCinemasByCityId', async () => {
+  const cinemas = await repository.getCinemasByCityId(cityId)
+  expect(Array.isArray(cinemas)).toBeTruthy()
+})
+
+
+test('getMoviesByCinemaId', async () => {
+  const movies = await repository.getMoviesByCinemaId(cinemaId)
+  console.log(movies)
+  expect(Array.isArray(movies)).toBeTruthy()
+  expect(movies.length).toBeTruthy()
 })
