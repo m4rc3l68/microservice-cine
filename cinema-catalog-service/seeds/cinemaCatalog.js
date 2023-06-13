570fa3a27d0dbefc112445b9a3d6753bac03862f
marcel <marcelinocost69@gmail.com>
2023-06-13 10:18:04 -0300

    commit - 14

--- cinema-catalog-service/seeds/cinemaCatalog.js
+++ cinema-catalog-service/seeds/cinemaCatalog.js
@@ -1,5 +1,3 @@
-const { ObjectId } = require("mongodb");
-
 const cinemaCatalog = [{cidade: "Gravataí",
 	uf: "RS",
 	cinemas: []
@@ -8,13 +6,13 @@ const cinemaCatalog = [{cidade: "Gravataí",
 	uf: "RS",
 	pais: "BR",
 	cinemas: [{
-		_id: new ObjectId("644ef9c58d5ec8f47755d06e"),
+		_id: ObjectId(),
 		nome: "Cinemark Bourbon Ipiranga",
 		salas: [{
 			nome: 1,
 			sessoes: [{
-				data: new Date("2023-05-01T09:00:00Z"),
-				idFilme: new ObjectId("644ef9c58d5ec8f47755d06e"),
+				data: ISODate("2023-05-01T09:00:00Z"),
+				idFilme: ObjectId("644ef9c58d5ec8f47755d06e"),
 				filme: "Vingadores: Guerra Infinita",
 				valor: 25.00,
 				assentos: [{
@@ -25,8 +23,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: false
 				}]
 			}, {
-				data: new Date("2023-05-01T11:00:00Z"),
-				idFilme: new ObjectId("644ef9c58d5ec8f47755d06e"),
+				data: ISODate("2023-05-01T11:00:00Z"),
+				idFilme: ObjectId("644ef9c58d5ec8f47755d06e"),
 				filme: "Vingadores: Guerra Infinita",
 				valor: 25.00,
 				assentos: [{
@@ -37,8 +35,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}, ]
 			},{
-				data: new Date("2022-06-01T13:00:00Z"),
-				idFilme: new ObjectId("644ef9dd8d5ec8f47755d06f"),
+				data: ISODate("2022-06-01T13:00:00Z"),
+				idFilme: ObjectId("644ef9dd8d5ec8f47755d06f"),
 				filme: "Vingadores: Era de Ultron",
 				valor: 20.00,
 				assentos: [{
@@ -55,8 +53,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 		}, {
 			nome: 2,
 			sessoes: [{
-				data: new Date("2023-05-01T09:00:00Z"),
-				idFilme: new ObjectId("644ef9dd8d5ec8f47755d06f"),
+				data: ISODate("2023-05-01T09:00:00Z"),
+				idFilme: ObjectId("644ef9dd8d5ec8f47755d06f"),
 				filme: "Vingadores: Era de Ultron",
 				valor: 25.00,
 				assentos: [{
@@ -67,8 +65,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: false
 				}, ]
 			}, {
-				data: new Date("2023-05-01T11:00:00Z"),
-				idFilme: new ObjectId("644ef9588d5ec8f47755d06d"),
+				data: ISODate("2023-05-01T11:00:00Z"),
+				idFilme: ObjectId("644ef9588d5ec8f47755d06d"),
 				filme: "Vingadores: Ultimato",
 				valor: 25.00,
 				assentos: [{
@@ -79,8 +77,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}, ]
 			}, {
-				data: new Date("2023-05-01T13:00:00Z"),
-				idFilme: new ObjectId("644ef9588d5ec8f47755d06d"),
+				data: ISODate("2023-05-01T13:00:00Z"),
+				idFilme: ObjectId("644ef9588d5ec8f47755d06d"),
 				filme: "Vingadores: Ultimato",
 				valor: 20.00,
 				assentos: [{
@@ -96,13 +94,13 @@ const cinemaCatalog = [{cidade: "Gravataí",
 			}]
 		}]
 	}, {
-		_id: new ObjectId("644ef9588d5ec8f47755d06d"),
+		_id: ObjectId(),
 		nome: "GNC Lindóia",
 		salas: [{
 			nome: 100,
 			sessoes: [{
-				data: new Date("2023-04-30T19:00:00Z"),
-				idFilme: new ObjectId("644ef9588d5ec8f47755d06d"),
+				data: ISODate("2023-04-30T19:00:00Z"),
+				idFilme: ObjectId("644ef9588d5ec8f47755d06d"),
 				filme: "Vingadores: Ultimato",
 				valor: 25.00,
 				assentos: [{
@@ -113,8 +111,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: false
 				},]
 			}, {
-				data: new Date("2023-04-30T11:00:00Z"),
-				idFilme: new ObjectId("644ef9588d5ec8f47755d06d"),
+				data: ISODate("2023-04-30T11:00:00Z"),
+				idFilme: ObjectId("644ef9588d5ec8f47755d06d"),
 				filme: "Vingadores: Ultimato",
 				valor: 25.00,
 				assentos: [{
@@ -125,8 +123,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}, ]
 			}, {
-				data: new Date("2023-04-30T13:00:00Z"),
-				idFilme: new ObjectId("644ef9dd8d5ec8f47755d06f"),
+				data: ISODate("2023-04-30T13:00:00Z"),
+				idFilme: ObjectId("644ef9dd8d5ec8f47755d06f"),
 				filme: "Vingadores: Era de Ultron",
 				valor: 20.00,
 				assentos: [{
@@ -140,8 +138,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}]
 			},{
-				data: new Date("2023-04-30T13:00:00Z"),
-				idFilme: new ObjectId("644efa048d5ec8f47755d071"),
+				data: ISODate("2023-04-30T13:00:00Z"),
+				idFilme: ObjectId("644efa048d5ec8f47755d071"),
 				filme: "Um lugar chamado Notting Hill",
 				valor: 25.00,
 				assentos: [{
@@ -152,8 +150,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}]
 			},{
-				data: new Date("2023-04-30T13:00:00Z"),
-				idFilme: new ObjectId("644efa148d5ec8f47755d072"),
+				data: ISODate("2023-04-30T13:00:00Z"),
+				idFilme: ObjectId("644efa148d5ec8f47755d072"),
 				filme: "Velozes e Furiosos - Opreração Rio",
 				valor: 30.00,
 				assentos: [{
@@ -164,8 +162,8 @@ const cinemaCatalog = [{cidade: "Gravataí",
 					disponivel: true
 				}]
 			},{
-				data: new Date("2023-04-30T13:00:00Z"),
-				idFilme: new ObjectId("644efa2d8d5ec8f47755d073"),
+				data: ISODate("2023-04-30T13:00:00Z"),
+				idFilme: ObjectId("644efa2d8d5ec8f47755d073"),
 				filme: "Top Gun - Uma Grande Aventura",
 				valor: 50.00,
 				assentos: [{
@@ -179,57 +177,3 @@ const cinemaCatalog = [{cidade: "Gravataí",
 		}]
 	}]
 }]
-
-function getAllCities() {
-  return cinemaCatalog.map(catalog => {
-		return {
-			_id: ObjectId(),
-			pais: catalog.pais,
-			uf: catalog.uf,
-			cidade: catalog.cidade
-		}
-	})
-}
-
-function getCinemasByCityId(cityId) {
-	return cinemaCatalog[cinemaCatalog.length -1].cinemas  
-}
-
-function getMoviesByCinemaId(cinemaId) {
-	return getCinemasByCityId().map(cinema => {
-		return {
-			titulo: cinema.salas[0].sessoes[0].filme,
-			_id: cinema.salas[0].sessoes[0].idFilme
-		}
-	})
-}
-
-function getMoviesByCityId(cityId){
- return getMoviesByCinemaId()
-}
-
-function getMovieSessionsByCityId(movieId, cityId) {
-		return getCinemasByCityId().map(cinema => {
-			return {
-				titulo: cinema.salas[0].sessoes[0].filme,
-				_id: cinema.salas[0].sessoes[0].idFilme,
-				cinema: cinema.nome,
-				idCinema: cinema._id,
-				sala: cinema.salas[0].nome,
-				sessao: cinema.salas[0].sessoes[0]
-			}
-	})
-}
-
-function getMovieSessionsByCinemaId(movieId, cinemaId) {
-	return getMovieSessionsByCityId()
-}
-
-module.exports = {
-  getAllCities,
-  getCinemasByCityId, 
-  getMoviesByCinemaId,
-  getMoviesByCityId,
-  getMovieSessionsByCityId,
-  getMovieSessionsByCinemaId
-}
